cd0c1456217d371d0c75426f7780940d0c01a028
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-10 12:16:43 +0100

    Warlock has and can change spell.

--- src/test/java/WarlockTest.java
+++ src/test/java/WarlockTest.java
@@ -1,2 +1,32 @@
+import org.junit.Before;
+import org.junit.Test;
+import players.magicians.Warlock;
+import spells.FireBall;
+import spells.LightningStrike;
+import spells.Spell;
+
+import static org.junit.Assert.assertEquals;
+
 public class WarlockTest {
+
+    Warlock warlock;
+    Spell spell;
+
+    @Before
+    public void before() throws Exception {
+        spell = new LightningStrike(5);
+        warlock = new Warlock("Saruman", 40, spell);
+    }
+
+    @Test
+    public void hasSpell() {
+        assertEquals(spell, warlock.getSpell());
+    }
+
+    @Test
+    public void canChangeSpell(){
+        Spell differentSpell = new FireBall(6);
+        warlock.setSpell(differentSpell);
+        assertEquals(differentSpell, warlock.getSpell());
+    }
 }
