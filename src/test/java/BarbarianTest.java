6a03bc556ea3136c0daa07bccd8b3d118bdb8910
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-10 14:07:42 +0100

    Dwarf and barbarian can attack. Tests passing.

--- src/test/java/BarbarianTest.java
+++ src/test/java/BarbarianTest.java
@@ -1,2 +1,27 @@
+import enemies.Orc;
+import org.junit.Before;
+import org.junit.Test;
+import players.heroes.Barbarian;
+import weapons.Club;
+
+import static org.junit.Assert.assertEquals;
+
 public class BarbarianTest {
+
+    Barbarian barbarian;
+    Club club;
+    Orc orc;
+
+    @Before
+    public void before() throws Exception {
+        club = new Club(4);
+        orc = new Orc("Bob", 10);
+        barbarian = new Barbarian("Bob", 20, club);
+    }
+
+    @Test
+    public void canAttack() {
+        barbarian.attack(orc);
+        assertEquals(16, orc.getHealth());
+    }
 }
--- src/test/java/DwarfTest.java
+++ src/test/java/DwarfTest.java
@@ -1,2 +1,29 @@
+import enemies.Orc;
+import org.junit.Before;
+import org.junit.Test;
+import players.heroes.Barbarian;
+import players.heroes.Dwarf;
+import weapons.Club;
+
+import static org.junit.Assert.assertEquals;
+
 public class DwarfTest {
+
+
+    Dwarf dwarf;
+    Club club;
+    Orc orc;
+
+    @Before
+    public void before() throws Exception {
+        club = new Club(4);
+        orc = new Orc("Bob", 10);
+        dwarf = new Dwarf("Bob", 20, club);
+    }
+
+    @Test
+    public void canAttack() {
+        dwarf.attack(orc);
+        assertEquals(16, orc.getHealth());
+    }
 }
